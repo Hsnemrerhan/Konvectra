a802978e7dfd4cfcde0a5a74d579b7ca58be52a4
Hasan Emre Erhan <emrerhan2003@hotmail.com>
2025-12-30 16:23:24 +0300

    vite allowed hosts config eklendi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,6 +20,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: [
+      "konvectra.com",
+      "www.konvectra.com"
+    ],
     watch: {
       usePolling: true
     }
